e6e70764d44f484ddbfaf7e50339c6d338713b4e
Sam Plews <sam@codeite.com>
2013-03-23 22:21:12 +0000

    Created Result and ValueResult<T>

---
+++ Codeite.Core/Result.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codeite.Core
+{
+    public class Result
+    {
+        public Result()
+        {}
+
+        public Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public IEnumerable<string> Messages()
+        {
+            var message = Message;
+            if (message == null)
+            {
+                return new string[0];
+            }
+
+            return message.Split(';').Select(x => x.Trim());
+        }        
+    }
+}
---
+++ Codeite.Core/ValueResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Codeite.Core
+{
+    public class ValueResult<T> : Result
+    {
+        public ValueResult()
+        {}
+
+        public ValueResult(bool success, string message, T value)
+            : base(success, message)
+        {
+            Value = value;   
+        }
+
+        public T Value { get; set; }
+
+    }
+}
