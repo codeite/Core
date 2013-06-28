08c400adbe86622b884a37cd351a458147104e88
Sam Plews <the@codeite.com>
2013-06-28 11:19:35 +0100

    Added Activator method missing from framework

---
+++ Codeite.Core/Activation/ActivatorHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Codeite.Core.Activation
+{
+    public static class ActivatorHelper
+    {
+        public static T CreateInstance<T>(params object[] args) where T : class
+        {
+            return Activator.CreateInstance(typeof (T), args) as T;
+        }
+    }
+}
